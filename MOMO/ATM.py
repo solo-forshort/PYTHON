35bffc27dd6b2619e00ace70add431ad778ee4c5
Solomon Asare <solomonasare0551.com>
2025-12-09 13:19:24 +0000

    Updating MOMO Folder

--- MOMO/ATM.py
+++ MOMO/ATM.py
@@ -12,28 +12,31 @@ print("2.Withdrawal")
 print("3.Check Balance")
 option = (int(input("Enter an option:  ")))
 
+# try:
 if option == 1:
-    amount = int(input("Enter an amount to deposit: "))
-    balance = balance + amount
-    print(f"Available balance is {balance}cedis")
+        amount = int(input("Enter an amount to deposit: "))
+        balance = balance + amount
+        print(f"Available balance is {balance}cedis")
 
 elif option == 2:
-    amount  = int(input("Enter amount to withdraw:  "))
+        amount  = int(input("Enter amount to withdraw:  "))
 
-    if amount > balance:
-        print("Insufficient or not enough balance ")
-    elif amount <= balance:
-        MM_PIN = int(input("Enter MM PIN to confirm: "))
-        balance = balance - amount
-        if MM_PIN == MoMo_PIN:
-                print(f"{amount}cedis successfully withdrawn ")
-                print(f"Balance: {balance}cedis ")
-        else :
-             print("Wrong/Incorrect PIN.. Try again")
+        if amount > balance:
+            print("Insufficient or not enough balance ")
+        elif amount <= balance:
+            MM_PIN = int(input("Enter MM PIN to confirm: "))
+            balance = balance - amount
+            if MM_PIN == MoMo_PIN:
+                    print(f"{amount}cedis successfully withdrawn ")
+                    print(f"Balance: {balance}cedis ")
+            else :
+                print("Wrong/Incorrect PIN.. Try again")
 
 elif option == 3:
-    MOMO_PIN = int(input("Enter MoMo PIN: "))
-    if MOMO_PIN == MoMo_PIN:
-        print(f"Available balance is {balance}cedis ")
-    else:
-        print("Incorrect MoMo PIN....!")
+        MOMO_PIN = int(input("Enter MoMo PIN: "))
+        if MOMO_PIN == MoMo_PIN:
+            print(f"Available balance is {balance}cedis ")
+        else:
+            print("Incorrect MoMo PIN....!")
+# except ValueError:
+#     print("Value Error! Enter values from 1 to 3")
